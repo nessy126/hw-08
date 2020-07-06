77e0c71e77aee520f341c1764877a5541e71fea7
nessy126 <nessy126@gmail.com>
2020-07-06 20:17:35 +0300

    Updates

--- portfolio.d4ad3f1f.js
+++ portfolio.d4ad3f1f.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55256" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58723" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
